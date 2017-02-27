3fa18e4f9e418a41728ee59d685e686d6e7d0f97
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-27 12:45:56 -0800

    finished wordcount added catch for non chars

--- wordcount.py
+++ wordcount.py
@@ -7,8 +7,16 @@ word_count = {}
 for line in text_file:
     stripped_line = line.rstrip()
     words = stripped_line.split(" ")
-    
+
+    all_letter_words = []
+
     for word in words:
+        if not word[-1].isalpha():
+            all_letter_words.append(word[:-1])
+        else:
+            all_letter_words.append(word)
+
+    for word in all_letter_words:
         word_count[word] = word_count.get(word, 0) + 1
 
 for word in word_count:
