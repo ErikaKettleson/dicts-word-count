390d3b2f2d04121f1d57daceed2e28fd506a2052
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-27 12:38:15 -0800

    WIP added words to dictionary and added print loop

--- wordcount.py
+++ wordcount.py
@@ -7,8 +7,11 @@ word_count = {}
 for line in text_file:
     stripped_line = line.rstrip()
     words = stripped_line.split(" ")
+    
+    for word in words:
+        word_count[word] = word_count.get(word, 0) + 1
 
-
-    print words
+for word in word_count:
+    print word, word_count[word]
 
 text_file.close()
