ac17392c395b0b12f0bc40a483c67128298cc932
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-27 13:05:10 -0800

    created function and added catch for upper/lower case words and printed a sorted dict

--- wordcount.py
+++ wordcount.py
@@ -1,25 +1,36 @@
 # put your code here.
 
-text_file = open("test.txt")
+def word_counter(text):
+    """ function to print a list of words and their frequency in a file
+    """
 
-word_count = {}
+    text_file = open(text)
 
-for line in text_file:
-    stripped_line = line.rstrip()
-    words = stripped_line.split(" ")
+    word_count = {}
 
-    all_letter_words = []
+    for line in text_file:
+        stripped_line = line.rstrip()
+        words = stripped_line.split(" ")
 
-    for word in words:
-        if not word[-1].isalpha():
-            all_letter_words.append(word[:-1])
-        else:
-            all_letter_words.append(word)
+        all_letter_words = []
 
-    for word in all_letter_words:
-        word_count[word] = word_count.get(word, 0) + 1
+        for word in words:
+            word = word.lower()
+            if not word[-1].isalpha():
+                all_letter_words.append(word[:-1])
+            else:
+                all_letter_words.append(word)
 
-for word in word_count:
-    print word, word_count[word]
+        for word in all_letter_words:
+            word_count[word] = word_count.get(word, 0) + 1
 
-text_file.close()
+    # for word in word_count:
+    #     print word, word_count[word]
+
+    sorted_word_count = sorted(word_count)
+    for word in sorted_word_count:
+        print word, word_count[word]
+
+    text_file.close()
+
+word_counter("test.txt")
